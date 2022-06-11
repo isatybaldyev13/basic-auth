75165852ff449646ef8e224979fc67c429f7c47b
isatybaldyev13 <iskender.satybaldyev@gmail.com>
2022-06-11 14:19:15 +0300

    created basic auth flow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import React from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  FormFeedback,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let email = "admin@gmail.com";
+let password = "123456";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isAuthenticated: false,
+      errorMsg: "",
+    };
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault(); //stop default action
+    let emailVal = event.target.elements.email.value;
+    let passwordVal = event.target.elements.password.value;
+    if (email === emailVal && password === passwordVal) {
+      this.setState({
+        isAuthenticated: true,
+      });
+      localStorage.setItem("isAuthenticated", "true");
+    } else {
+      this.setState({
+        errorMsg: "Please enter valid credentials",
+      });
+    }
+  };
+
+  handleSignOut = ()=>{
+    this.setState({
+      isAuthenticated: false
+    })
+    localStorage.removeItem("isAuthenticated")
+  }
+
+  componentDidMount() {
+    console.log("app is started");
+    let auth = localStorage.getItem("isAuthenticated");
+    this.setState({
+      isAuthenticated: auth ? true : false,
+    });
+  }
+
+  //request types
+
+  //GET, POST, DELETE, PUT,
+
+  render() {
+    const { isAuthenticated, errorMsg } = this.state;
+    return isAuthenticated ? (
+      <Container>
+        <h1>home page</h1>
+        <Button onClick={this.handleSignOut}>Sign out</Button>
+      </Container>
+    ) : (
+      <Container>
+        <Col md="6">
+          <Form className="mt-5" onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Label for="exampleEmail">Email</Label>
+              <Input
+                id="exampleEmail"
+                name="email"
+                placeholder="with a placeholder"
+                type="email"
+                invalid={errorMsg !== ""}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                id="examplePassword"
+                name="password"
+                placeholder="password placeholder"
+                type="password"
+                invalid={errorMsg !== ""}
+              />
+              {errorMsg !== "" && <FormFeedback>{errorMsg}</FormFeedback>}
+            </FormGroup>
+            <Button>Sign in</Button>
+          </Form>
+        </Col>
+      </Container>
+    );
+  }
 }
 
 export default App;
